b6ec55e97aef5291153dd5a2357c2462b81eaadc
dantespe <dantespe@umich.edu>
2017-02-03 17:33:08 -0500

    Changes default behavior

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 test:
-	g++ -stdlib=libc++ --std=c++11 main.cpp Passgen.cpp -o passwords.exe
+	g++ --std=c++11 main.cpp Passgen.cpp -o passwords.exe
 
 clean:
 	rm -rf *.exe *.out
--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <GetOpt.h>
+#include <unistd.h>
 using namespace std;
 
-const bool INCLUDE_NUMEBRS_DEFAULT = false;
-const bool INCLUDE_SYMBOLS_DEFAULT = false;
+const bool INCLUDE_NUMEBRS_DEFAULT = true;
+const bool INCLUDE_SYMBOLS_DEFAULT = true;
 const int PASSWORD_DEFAULT_LENGTH = 128;
 const int PASSWORDS_TO_WRITE = 1;
 const char *VERBOSE =
@@ -44,22 +44,37 @@ void interactive(Passgen & passgen) {
     return;
 }
 
+void print_computation(bool def, int count, int length,
+  bool numbers, bool symbols) {
+    if (def)
+        cout << "USING DEFAULT SETTINGS:\n";
+
+    cout << "Generating " << count << " password(s).\n"
+         << "Length: " << length << " characters.\n";
+
+    numbers ? cout << "Including Numbers\n" :
+        cout << "Excluding Numbers\n";
+
+    symbols ? cout << "Including Symbols\n" :
+        cout << "Excluding Symbols\n";
+
+    cout << "===========================\n";
+}
+
 int main(int argc, char * argv[]) {
     srand(time(0)); //Needed to generate "random" numbers
     bool numbers(INCLUDE_NUMEBRS_DEFAULT), symbols(INCLUDE_SYMBOLS_DEFAULT);
     int num_passwords_to_write(PASSWORDS_TO_WRITE);
     int password_length(PASSWORD_DEFAULT_LENGTH);
 
-    //Create PAssgen
-    if argc == 1;
-    int operation;
-
-    while ((operation = getopt(argc, argv, "c:il:ns") != EOF) {
-        switch (operation) {
-            case 'c':
-                num_passwords_to_write
-        }
+    Passgen passgen(password_length, numbers, symbols);
+    if (argc == 1) {
+      print_computation(true, num_passwords_to_write, password_length,
+          numbers, symbols);
+      passgen.pass_to_write = num_passwords_to_write;
+      passgen.write_passwords();
+      return 0;
     }
-    passgen.write_passwords();
-    return 0;
+
+  return 0;
 }
